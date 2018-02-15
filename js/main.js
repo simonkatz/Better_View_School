6733fe15bf0b228b2d52374fafd4ac3f0a68dc48
Simon Kateregga <spkateregga@gmail.com>
2018-02-15 10:58:06 -0600

    Website Version 2.0

--- js/main.js
+++ js/main.js
@@ -1,84 +1,55 @@
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+    /* ===================================
+     swiper slider
+     ====================================== */
+    var swiperFull = new Swiper('.swiper-full-screen', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        loop: true,
+        autoplay: 5000,
+        slidesPerView: 1,
+        keyboardControl: true,
+        preventClicks: false,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
     });
-});
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+    var swiperMultyRow = new Swiper('.swiper-multy-row-container', {
+        nextButton: '.swiper-portfolio-next',
+        prevButton: '.swiper-portfolio-prev',
+        slidesPerView: 2,
+        spaceBetween: 15,
+        autoplay: 3000,
+        autoplayDisableOnInteraction: true,
+        breakpoints: {
+            991: {
+                slidesPerView: 1
+            },
+        }
+    });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+    /*==============================================================
+     wow animation - on scroll
+     ==============================================================*/
+    var wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 0,
+        mobile: false,
+        live: true
+    });
+    wow.init();
 
-//implement bx-slider
-$('.facility').bxSlider({
-  mode: 'fade',
-  auto: true,
-  controls: false,
-  captions: true,
-  pager: true,
-  pagerCustom: '#bx-facility'
-});
-
-//implement bx-slider
-$('.pupils').bxSlider({
-  mode: 'fade',
-  auto: true,
-  controls: false,
-  captions: true,
-  pager: true,
-  pagerCustom: '#bx-pupils'
-});
-
-//implement bx-slider
-$('.extra').bxSlider({
-  mode: 'fade',
-  auto: true,
-  controls: false,
-  captions: true,
-  pager: true,
-  pagerCustom: '#bx-extra'
-});
-
-
-$('header .slider').slider(
-    {
-        full_width: true,
-        indicators: true,
-        height: 700,
-        no_wrap: true
-    }
-);
-    
-$('.hours .slider').slider(
-    {
-        full_width: true,
-        indicators: true,
-        height: 600,
-        no_wrap: true
-    }
-);
-$('#fullpage').fullpage({
-    sectionsColor: ['white', '#F5F5F5'],
-    loopHorizontal: false,
-    anchors:['firstPage', 'secondPage', 'last'],
-    paddingTop: '30px',
-    paddingBottom: '30px',
-    fitToSection: false,
-    autoScrolling: false,
-    paddingTop: 0,
-    paddingBottom: 0,
-});
- 
-
-$('.swipebox').swipebox();
+    $('.lightbox-portfolio').magnificPopup({
+        delegate: '.gallery-link',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-fade',
+        fixedContentPos: true,
+        closeBtnInside: false,
+        gallery: {
+            enabled: true,
+            navigateByImgClick: false,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        }
+    });
